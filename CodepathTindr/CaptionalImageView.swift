70456ddefaa980ac31a6f87de0a02515ba68db41
zhidev <Doug@douglass-macbook-air.local>
2016-03-24 20:00:35 -0700

    Milestone4

---
+++ CodepathTindr/CaptionalImageView.swift
@@ -0,0 +1,21 @@
+//
+//  CaptionalImageView.swift
+//  CodepathTindr
+//
+//  Created by Douglas on 3/24/16.
+//  Copyright © 2016 Dougli. All rights reserved.
+//
+
+import UIKit
+
+class CaptionalImageView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ CodepathTindr/CardsViewController.swift
@@ -0,0 +1,44 @@
+//
+//  CardsViewController.swift
+//  CodepathTindr
+//
+//  Created by Douglas on 3/24/16.
+//  Copyright © 2016 Dougli. All rights reserved.
+//
+
+import UIKit
+
+class CardsViewController: UIViewController {
+
+    
+    @IBOutlet var panGestureRecognizer: UIPanGestureRecognizer!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func onPanGesture(sender: UIPanGestureRecognizer) {
+        /*let point = sender.locationInView(self.view)
+        var initial = avatar.center
+        if sender.state == UIGestureRecognizerState.Began{
+            initial = avatar.center
+        }else if sender.state == UIGestureRecognizerState.Changed{
+            UIView.animateWithDuration(1, animations: { () -> Void in
+                self.avatar.center = CGPoint(x:point.x , y: initial.y)
+            })
+        }else{
+            
+        }*/
+    }
+
+
+}
